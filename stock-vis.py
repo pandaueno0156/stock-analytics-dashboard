f590db0869f8368a1e0ee4dc19794bb6abf11067
pandaueno0156 <pandaueno0156@gmail.com>
2025-04-04 10:29:47 +1100

    Implemented post-pre stock price, current NY time

--- stock-vis.py
+++ stock-vis.py
@@ -3,6 +3,9 @@ import yfinance as yf
 import plotly.express as px
 import pandas as pd
 from GoogleNews import GoogleNews
+from datetime import datetime
+import pytz
+
 
 st.set_page_config(
     page_title = "Stock Analytics Dashboard",
@@ -36,6 +39,8 @@ def fetch_stock_data(stock, start, end):
     ticker = yf.Ticker(stock)
     stock_info = ticker.info
 
+    print(f'stock_info: {stock_info}')
+
     # Get specific financial data we need
     financial_data = {
         'financials': ticker.financials.loc['Total Revenue'].to_dict() if not ticker.financials.loc['Total Revenue'].empty else {},
@@ -51,7 +56,61 @@ def fetch_stock_data(stock, start, end):
     df = df[start:end]
     return df, stock_info, financial_data
 
+def get_ny_time_status():
+    # NY time
+    ny_time_zone = pytz.timezone('America/New_York')
+    ny_time = datetime.now(ny_time_zone)
+    
+
+    # Check if NY is weekday
+    # This should be 0 - 4 to be weekday
+    is_weekday = ny_time.weekday() < 5
+
+    # Cehck if time is 9:30 - 4:00 pm NY time
+    pre_market_start = ny_time.replace(hour=4, minute=0, second=0) # 4:00 AM ET
+    market_open_time = ny_time.replace(hour=9, minute=30, second=0) # 9:30 AM ET
+    market_close_time = ny_time.replace(hour=16, minute=0, second=0) # 4:00 PM ET
+    is_market_hours = market_open_time <= ny_time <= market_close_time
+
+    # Time format
+    time_str = ny_time.strftime("%I:%M:%S %p Eastern Time")
+
+    # Identify the market status
+    if not is_weekday: # Market closed still shows post-market price
+        status = "Market Closed (Weekend)"
+        color = "red"
+    elif pre_market_start <= ny_time < market_open_time:
+        status = "Pre-Market"
+        color = "skyblue"
+    elif market_open_time <= ny_time < market_close_time:
+        status = "Market Open"
+        color = "green"
+    elif market_close_time <= ny_time or ny_time < pre_market_start:
+        status = "After Hours"
+        color = "skyblue"
+    return time_str, status, color
+
 df, stock_info, financial_data = fetch_stock_data(selected_stock, start_date, end_date)
+
+
+# Display market time and status
+col1, col2 = st.columns(2)
+time_str, market_status, color = get_ny_time_status()
+
+with col1:
+    st.markdown(f"<h3 style='margin-bottom: 0px'>{time_str}</h3>", unsafe_allow_html=True)
+
+with col2:
+    icons = {
+        "Market Closed (Weekend)": "🔒",
+        "Market Open": "🔆",
+        "After Hours": "🌙",
+        "Pre-Market": "🌅"
+    }
+    icon = icons.get(market_status, "")
+
+    st.markdown(f"<h5 style='color: {color}; margin-bottom: 0px; margin-top: 12px;'>{market_status} {icon}</h5>", unsafe_allow_html=True)
+
 
 # Annual and Quarter Financials
 financials_dict = financial_data['financials']
@@ -64,11 +123,11 @@ quarter_earnings = pd.DataFrame(list(financial_data['quarterly_earnings'].items(
 # print(f'stock_info: {stock_info}')
 
 # Create columns for metrics
-col1, col2, col3 = st.columns(3)
-col4, col5, col6 = st.columns(3)
-col7, col8, col9, col10 = st.columns(4)
+col1, col2, col3, col4 = st.columns(4)
+col5, col6, col7 = st.columns(3)
+col8, col9, col10, col11 = st.columns(4)
 
-# Current Price and Change
+# Current and Post Price and Change
 latest_price = float(df['Close'].iloc[-1][selected_stock])
 price_change = ((df['Close'].iloc[-1][selected_stock] - df['Close'].iloc[-2][selected_stock]) / df['Close'].iloc[-2][selected_stock]) * 100
 
@@ -76,40 +135,71 @@ price_change = ((df['Close'].iloc[-1][selected_stock] - df['Close'].iloc[-2][sel
 with col1:
     st.metric(label="Stock Price", value=round(latest_price, 2), delta=f"{price_change:.2f}%")
 
-# PE Ratio
+# Post Stock Price
 with col2:
+    # Only show this information if the market is in After hour
+    if market_status == 'After Hours':
+        post_latest_price = stock_info.get('postMarketPrice', 'N/A')
+        post_price_change = stock_info.get('postMarketChange', 'N/A')
+        if post_latest_price is not None and post_price_change is not None:
+
+            st.metric(label="Post Stock Price", value=round(post_latest_price, 2), delta=f"{post_price_change:.2f}%")
+        else:
+            st.metric(label="Post Stock Price", value='N/A', delta=None)
+    elif market_status == 'Market Closed (Weekend)':
+        post_latest_price = stock_info.get('postMarketPrice', 'N/A')
+        post_price_change = stock_info.get('postMarketChange', 'N/A')
+        if post_latest_price is not None and post_price_change is not None:
+
+            st.metric(label="Post Stock Price", value=round(post_latest_price, 2), delta=f"{post_price_change:.2f}%")
+        else:
+            st.metric(label="Post Stock Price", value='N/A', delta=None)
+    elif market_status == 'Pre-Market':
+        pre_latest_price = stock_info.get('preMarketPrice', 'N/A')
+        pre_price_change = stock_info.get('preMarketChange', 'N/A')
+        if pre_latest_price is not None and pre_price_change is not None:
+
+            st.metric(label="Pre Stock Price", value=round(pre_latest_price, 2), delta=f"{pre_price_change:.2f}%")
+        else:
+            st.metric(label="Pre Stock Price", value='N/A', delta=None)
+    elif market_status == 'Market Open':
+        # Post-Pre Stock Price will not show up during market open
+        st.metric(label="Post Stock Price", value='N/A', delta=None)
+
+# 50-day MA comparison
+with col3:
+    ma50_latest = float(df['MA50'].iloc[-1])
+    ma50_diff = ((latest_price - ma50_latest) / ma50_latest) * 100
+    st.metric(label="50-Day MA", value=f"${ma50_latest:.2f}", delta=f"{ma50_diff:.2f}%")
+
+# 200-day MA comparison
+with col4:
+    ma200_latest = float(df['MA200'].iloc[-1])
+    ma200_diff = ((latest_price - ma200_latest) / ma200_latest) * 100
+    st.metric(label="200-Day MA", value=f"${ma200_latest:.2f}", delta=f"{ma200_diff:.2f}%")
+
+# PE Ratio
+with col5:
     pe_ratio = stock_info.get('trailingPE', 'N/A')
     if pe_ratio != 'N/A':
         pe_ratio = f"{pe_ratio:.2f}"
     st.metric(label="P/E Ratio", value=pe_ratio)
 
 # EPS
-with col3:
+with col6:
     # trailing twelve months
     eps = stock_info.get('trailingEps', 'N/A')
     if eps != 'N/A':
         eps = f"${eps:.2f}"
     st.metric(label="EPS (TTM)", value=eps)
 
-# 50-day MA comparison
-with col4:
-    ma50_latest = float(df['MA50'].iloc[-1])
-    ma50_diff = ((latest_price - ma50_latest) / ma50_latest) * 100
-    st.metric(label="50-Day MA", value=f"${ma50_latest:.2f}", delta=f"{ma50_diff:.2f}%")
-
-# 200-day MA comparison
-with col5:
-    ma200_latest = float(df['MA200'].iloc[-1])
-    ma200_diff = ((latest_price - ma200_latest) / ma200_latest) * 100
-    st.metric(label="200-Day MA", value=f"${ma200_latest:.2f}", delta=f"{ma200_diff:.2f}%")
-
 # Beta
-with col6:
+with col7:
     beta = stock_info.get('beta', 'N/A')
     st.metric(label="Beta", value=beta)
 
 # Annual Revenue Growth Comparison
-with col7:
+with col8:
     latest_year_revenue = financials.iloc[0].values[0]
     previous_year_revenue = financials.iloc[1].values[0]
     annual_revenue_growth = ((latest_year_revenue - previous_year_revenue) / previous_year_revenue) * 100
@@ -117,21 +207,21 @@ with col7:
     st.metric(label="Annual Revenue Growth", value=f"{annual_revenue_growth:.2f}%")
 
 # Quarterly Revenue Growth Comparison
-with col8:
+with col9:
     latest_quarter_revenue = quarter_financials.iloc[0].values[0]
     previous_quarter_revenue = quarter_financials.iloc[1].values[0]
     quarterly_revenue_growth = ((latest_quarter_revenue - previous_quarter_revenue) / previous_quarter_revenue) * 100
     st.metric(label="Quarter Revenue Growth", value=f"{quarterly_revenue_growth:.2f}%")
 
 # Annual Earning Growth Comparison
-with col9:
+with col10:
     latest_yearly_earning = earnings.iloc[0].values[0]
     previous_yearly_earning = earnings.iloc[1].values[0]
     annual_earning_growth = ((latest_yearly_earning - previous_yearly_earning) / previous_yearly_earning) * 100
     st.metric(label="Annual Earning Growth", value=f"{annual_earning_growth:.2f}%")
 
 # Quarterly Earning Growth Comparison
-with col10:
+with col11:
     latest_quarterly_earning = quarter_earnings.iloc[0].values[0]
     previous_quarterly_earning = quarter_earnings.iloc[1].values[0]
     quarterly_earning_growth = ((latest_quarterly_earning - previous_quarterly_earning) / previous_quarterly_earning) * 100
